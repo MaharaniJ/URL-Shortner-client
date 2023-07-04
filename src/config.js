d3ae50202854738bb7512b2054a1f1b557885ffd
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-07-04 17:59:51 +0530

    vercel-deployement

--- src/config.js
+++ src/config.js
@@ -1,11 +1,5 @@
 export const env = {
-     api : "http://localhost:5000/auth",
-     api1 : "http://localhost:5000/api",
-     redirect : "http://localhost:5000/api/shortUrl"
-
-    // api : "https://url-shortener-backend-ten.vercel.app/auth",
-    // api1 : "https://url-shortener-backend-ten.vercel.app/api",
-    // redirect : "https://url-shortener-backend-ten.vercel.app/api/shortUrl"
-    
-    
+     api : "https://url-shortener-server-iota.vercel.app//auth",
+     api1 : "https://url-shortener-server-iota.vercel.app//api",
+     redirect : "https://url-shortener-server-iota.vercel.app/api/shortUrl"
 }
